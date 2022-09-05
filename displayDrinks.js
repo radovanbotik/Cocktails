d3abda431e1d98177b3f956f8f82887e3488abf0
radovanbotik <radovanbotik@gmail.com>
2022-09-05 15:52:28 +0200

    loading and fail handled

--- displayDrinks.js
+++ displayDrinks.js
@@ -1,16 +1,26 @@
 import { fetchDrinks } from "./fetchDrinks.js";
 import { createDrinks } from "./createDrinks.js";
+import { getElement } from "./utility/getElement.js";
+
+const bodyloader = getElement(".bodyloader");
+const bodyannouncer = getElement(".bodyannouncer");
+const drinksbody = getElement(".drinksbody");
 
 const displayDrinks = async url => {
+  bodyloader.classList.remove("hidden");
   const fetchedDrinks = await fetchDrinks(url);
   if (fetchedDrinks) {
     //animation
-    //title
+    bodyloader.classList.add("hidden");
     //html
     createDrinks(fetchedDrinks);
   } else {
     //animation
+    bodyloader.classList.add("hidden");
     //title
+    bodyannouncer.remove("hidden");
+    //no results displayed - reset dom
+    drinksbody.innerHTML = null;
   }
 };
 
