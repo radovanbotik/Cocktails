9b1769873c5657e89a6c57ab6208da54c8896b9c
radovanbotik <radovanbotik@gmail.com>
2022-09-06 14:15:49 +0200

    added logic to display single drink, and logic to display ingredients dynamically

---
+++ createSingleDrink.js
@@ -0,0 +1,41 @@
+import { getElement } from "./utility/getElement.js";
+
+const createSingleDrink = drinkdata => {
+  const { drinks } = drinkdata;
+  const ingredientsArray = Object.keys(drinks[0]).filter(key => {
+    return key.includes("strIngredient");
+  });
+  console.log(ingredientsArray);
+  const ingredients = ingredientsArray.map(ingredient => {
+    return drinks[0][ingredient];
+  });
+  const ingredientsInUse = ingredients.filter(ingredient => ingredient);
+  const ingredientsListItems = ingredientsInUse
+    .map(ingredient => {
+      return `
+    <li>💥${ingredient}</li>
+    `;
+    })
+    .join("");
+  const {
+    idDrink: id,
+    strDrinkThumb: image,
+    strDrink: name,
+    strAlcoholic: category,
+    strInstructions: instructions,
+  } = drinks[0];
+  const drinkcontainer = getElement(".drinkcontainer");
+  drinkcontainer.innerHTML = `
+<img src="${image}" alt="${name}" class="drinkimage" />
+<article class="drinkdata">
+  <h2 class="drinkname">${name}</h2>
+  <h4 class="drinkcategory">${category}</h4>
+  <p class="drinkinstructions">${instructions}.</p>
+  <ul class="drinkingredients">
+    ${ingredientsListItems}
+  </ul>
+  <a href="index.html" class="returnbutton">return to search</a>
+`;
+};
+
+export { createSingleDrink };
--- loadDrink.js
+++ loadDrink.js
@@ -1,3 +1,6 @@
+import { fetchDrinks } from "./fetchDrinks.js";
+import { createSingleDrink } from "./createSingleDrink.js";
+
 const loadDrink = drinksDOM => {
   drinksDOM.addEventListener("click", e => {
     e.stopImmediatePropagation();
@@ -6,4 +9,15 @@ const loadDrink = drinksDOM => {
     console.log(targetsID);
   });
 };
+const baseURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+const retrievedID = localStorage.getItem("selecteddrink");
+
+const retrieveDrink = async () => {
+  const drinkdata = await fetchDrinks(`${baseURL}${retrievedID}`);
+  const drink = createSingleDrink(drinkdata);
+};
+retrieveDrink();
+// const drinkdata = fetchDrinks(`${baseURL}${retrievedID}`);
+// console.log(drinkdata);
+
 export { loadDrink };
